4bd4e729bd14a327255cc042889b35fe533da77d
Thalassocracy <42811940+Thalassocracy@users.noreply.github.com>
2018-12-02 23:38:32 -0500

    functional Alcor HS for 6v6
    needs 7v7 support and description revision

--- ui/mods/icarus/maps.js
+++ ui/mods/icarus/maps.js
@@ -2,5 +2,9 @@ cShareSystems.load_pas("ICARUS Map Pack", [
     "coui://ui/mods/icarus/systems/[icarus]vulgar masses 5v5 ns.pas",
     "coui://ui/mods/icarus/systems/[icarus]vortex valley 6v6 ns.pas",
     "coui://ui/mods/icarus/systems/[icarus]the dried lake 7v7 ns.pas",
-    "coui://ui/mods/icarus/systems/[icarus]juggernaut 4v4v4 or 6x2 ns.pas"
+    "coui://ui/mods/icarus/systems/[icarus]juggernaut 4v4v4 or 6x2 ns.pas",
+	"coui://ui/mods/icarus/systems/[icarus]Alcor HS.pas"
+],
+"ICARUS Hemispheric Spawn", [
+	"coui://ui/mods/icarus/systems/[icarus]Alcor HS.pas"
 ]);
