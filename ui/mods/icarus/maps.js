14d7ee15d9f6af18f42f7f78c17f3551b14739a2
Thalassocracy <42811940+Thalassocracy@users.noreply.github.com>
2020-05-27 15:57:30 -0400

    reorganized map tabs

--- ui/mods/icarus/maps.js
+++ ui/mods/icarus/maps.js
@@ -1,4 +1,4 @@
-cShareSystems.load_pas("ICARUS - <10 Players", [
+cShareSystems.load_pas("ICARUS: 1v1 - 5v5 Players", [
     "coui://ui/mods/icarus/systems/monkey/execration_1v1 ns.pas",
     "coui://ui/mods/icarus/systems/anon/regolith&steel_1v1_ns.pas",
     "coui://ui/mods/icarus/systems/anon/locket_2v2_ns.pas",
@@ -9,7 +9,6 @@ cShareSystems.load_pas("ICARUS - <10 Players", [
     "coui://ui/mods/icarus/systems/anon/monstribus_3v3 ns.pas",
     "coui://ui/mods/icarus/systems/anon/porcupine_3v3_ns.pas",
     "coui://ui/mods/icarus/systems/anon/rafters_3v3 ns.pas",
-    "coui://ui/mods/icarus/systems/anon/atree'splace ds 3v3-5v5.pas",
     "coui://ui/mods/icarus/systems/anon/appalachian_4v4 ns.pas",
     "coui://ui/mods/icarus/systems/anon/fission_4v4 ns.pas",
     "coui://ui/mods/icarus/systems/anon/fuse_4v4 ns.pas",
@@ -19,8 +18,6 @@ cShareSystems.load_pas("ICARUS - <10 Players", [
     "coui://ui/mods/icarus/systems/anon/the_strip_4v4_ns.pas",
     "coui://ui/mods/icarus/systems/anon/stiring_4v4_ns.pas",
     "coui://ui/mods/icarus/systems/anon/telco_4v4_ns.pas",
-    "coui://ui/mods/icarus/systems/diss/arid_lake hs 4v4-5v5.pas",
-    "coui://ui/mods/icarus/systems/diss/burial_chambers hs 4v4-5v5.pas",
     "coui://ui/mods/icarus/systems/anon/battle_of midway 5v5 ns.pas",
     "coui://ui/mods/icarus/systems/kiwi/chungus_5v5_v2.pas",
     "coui://ui/mods/icarus/systems/anon/divesmetallum_5v5.pas",
@@ -35,67 +32,79 @@ cShareSystems.load_pas("ICARUS - <10 Players", [
     "coui://ui/mods/icarus/systems/anon/sedimentary_5v5 ns.pas",
     "coui://ui/mods/icarus/systems/monkey/vulgar_masses_5v5 ns.pas",
     "coui://ui/mods/icarus/systems/anon/westward_5v5 ns.pas",
-    "coui://ui/mods/icarus/systems/diss/beach_hs_4v4-6v6.pas",
-    "coui://ui/mods/icarus/systems/anon/dunes ds 4v4-6v6.pas",
-    "coui://ui/mods/icarus/systems/anon/beasts_ds 4v4-8v8.pas",
-    "coui://ui/mods/icarus/systems/diss/fools_journey_ds 4v4-10v10.pas",
-    "coui://ui/mods/icarus/systems/anon/pebbles_ds 4v4-8v8.pas",
-    "coui://ui/mods/icarus/systems/anon/threepaths ds 4x1-4x3.pas",
-    "coui://ui/mods/icarus/systems/anon/spark.pas",
-    "coui://ui/mods/icarus/systems/diss/watreeya 2.pas",
 ]);
 
-cShareSystems.load_pas("ICARUS - 11+ Players", [
+cShareSystems.load_pas("ICARUS: 11+ Players", [
     "coui://ui/mods/icarus/systems/monkey/hell_wars_6v6 ns.pas",
     "coui://ui/mods/icarus/systems/anon/icon_6v6_ns.pas",
     "coui://ui/mods/icarus/systems/diss/the_ledge_6v6 ns.pas",
     "coui://ui/mods/icarus/systems/anon/xpres_6v6 ns.pas",
-    "coui://ui/mods/icarus/systems/diss/alcor_6v6_7v7 ds.pas",
-    "coui://ui/mods/icarus/systems/anon/system-shock_ds 5v5-10v10.pas",
-    "coui://ui/mods/icarus/systems/anon/tale_of_two_worlds ds 6v6-8v8.pas",
     "coui://ui/mods/icarus/systems/diss/the_dried_lake_7v7 ns.pas",
     "coui://ui/mods/icarus/systems/diss/lava_chamber_7v7 ns.pas",
     "coui://ui/mods/icarus/systems/monkey/lion_killer_7v7 ns.pas",
     "coui://ui/mods/icarus/systems/anon/zpra_7v7 ns.pas",
     "coui://ui/mods/icarus/systems/diss/the_blood_aqueduct_8v8 ns.pas",
     "coui://ui/mods/icarus/systems/anon/bury_the ant_8v8-4x4_ns.pas",
-    "coui://ui/mods/icarus/systems/anon/system-double_ds 8v8-10v10.pas",
     "coui://ui/mods/icarus/systems/monkey/fake_history_9v9 ns.pas",
-    "coui://ui/mods/icarus/systems/anon/system_re-boot ds 8v8-16v16.pas",
     "coui://ui/mods/icarus/systems/anon/clockwork_10v10-4x5_ns.pas",
-    "coui://ui/mods/icarus/systems/anon/system-crash ds 10v10-16v16.pas",
+
+    "coui://ui/mods/icarus/systems/diss/juggernaut_6x2_4v4v4 ns.pas",
     "coui://ui/mods/icarus/systems/anon/squabble_4v4v4 ns.pas",
-    "coui://ui/mods/icarus/systems/anon/system-surge ds 3x3-3x5.pas",
     "coui://ui/mods/icarus/systems/anon/gizmos_8v8v8_ns.pas",
     "coui://ui/mods/icarus/systems/anon/gateway_5v5v5 ns.pas",
-    "coui://ui/mods/icarus/systems/anon/armadillo_3v3v3v3 ns.pas",
-    "coui://ui/mods/icarus/systems/anon/tables_3v3v3v3 ns.pas",
-    "coui://ui/mods/icarus/systems/anon/excursion_4x6_ns.pas",
-    "coui://ui/mods/icarus/systems/anon/chasm_5x4_ns.pas",
-    "coui://ui/mods/icarus/systems/diss/juggernaut_6x2_4v4v4 ns.pas",
-    "coui://ui/mods/icarus/systems/anon/glyptodon_6x3 ns.pas",
     "coui://ui/mods/icarus/systems/anon/ragnarok_6x3_6x2 ns.pas",
-    "coui://ui/mods/icarus/systems/anon/threepaths_x3 ds 12x1-12x3.pas",
+
+    "coui://ui/mods/icarus/systems/anon/armadillo_3v3v3v3 ns.pas",
+    "coui://ui/mods/icarus/systems/anon/excursion_4x6_ns.pas",
+
+    "coui://ui/mods/icarus/systems/anon/chasm_5x4_ns.pas",
+    
+    "coui://ui/mods/icarus/systems/anon/glyptodon_6x3 ns.pas",
+    
     "coui://ui/mods/icarus/systems/anon/intrepid_16 ffa.pas",
     "coui://ui/mods/icarus/systems/anon/kuiper_18 ffa.pas",
     "coui://ui/mods/icarus/systems/anon/guns_24 ffa.pas",
     "coui://ui/mods/icarus/systems/anon/tirade_24 ffa.pas",
     "coui://ui/mods/icarus/systems/anon/kersmash_32 ffa.pas",
     "coui://ui/mods/icarus/systems/anon/the_maul 32 ffa.pas",
+]);
+
+cShareSystems.load_pas("ICARUS: Variable Size", [
+    "coui://ui/mods/icarus/systems/anon/spark.pas",
+    "coui://ui/mods/icarus/systems/diss/watreeya 2.pas",
     "coui://ui/mods/icarus/systems/diss/big_wadiya.pas",
     "coui://ui/mods/icarus/systems/anon/flash.pas",
     "coui://ui/mods/icarus/systems/diss/greater_wadiya.pas",
     "coui://ui/mods/icarus/systems/anon/volt.pas",
     "coui://ui/mods/icarus/systems/anon/noiban_ffa.pas",
+
+    "coui://ui/mods/icarus/systems/anon/atree'splace ds 3v3-5v5.pas",
+    "coui://ui/mods/icarus/systems/diss/arid_lake hs 4v4-5v5.pas",
+    "coui://ui/mods/icarus/systems/diss/burial_chambers hs 4v4-5v5.pas",
+    "coui://ui/mods/icarus/systems/diss/beach_hs_4v4-6v6.pas",
+    "coui://ui/mods/icarus/systems/anon/dunes ds 4v4-6v6.pas",
+    "coui://ui/mods/icarus/systems/anon/beasts_ds 4v4-8v8.pas",
+    "coui://ui/mods/icarus/systems/diss/fools_journey_ds 4v4-10v10.pas",
+    "coui://ui/mods/icarus/systems/anon/pebbles_ds 4v4-8v8.pas",
+    "coui://ui/mods/icarus/systems/diss/alcor_6v6_7v7 ds.pas",,
+    "coui://ui/mods/icarus/systems/anon/system-shock_ds 5v5-10v10.pas",
+    "coui://ui/mods/icarus/systems/anon/tale_of_two_worlds ds 6v6-8v8.pas",
+    "coui://ui/mods/icarus/systems/anon/system-double_ds 8v8-10v10.pas",
+    "coui://ui/mods/icarus/systems/anon/system_re-boot ds 8v8-16v16.pas",
+    "coui://ui/mods/icarus/systems/anon/system-crash ds 10v10-16v16.pas",
+
+    "coui://ui/mods/icarus/systems/anon/system-surge ds 3x3-3x5.pas",
+    
+    "coui://ui/mods/icarus/systems/anon/threepaths ds 4x1-4x3.pas"
+
 ]);
 
-cShareSystems.load_pas("ICARUS - Legacy", [
+cShareSystems.load_pas("ICARUS: Legacy", [
     "coui://ui/mods/icarus/systems/anon/graveyardshift_3v3_ns.pas",
     "coui://ui/mods/icarus/systems/monkey/prison_colony_4v4 ns.pas",
-    
     "coui://ui/mods/icarus/systems/monkey/war_infection_5v5 ns.pas",
     "coui://ui/mods/icarus/systems/monkey/dirge_field_6v6 ns.pas",
-    
     "coui://ui/mods/icarus/systems/monkey/colder_logic_8v8 ns.pas",
     "coui://ui/mods/icarus/systems/monkey/killbox_10v10 ns.pas",
+    "coui://ui/mods/icarus/systems/anon/tables_3v3v3v3 ns.pas",
 ]);
